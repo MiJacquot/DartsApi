e2eefb7b18d10d1f5f4632de9846c5c554146a7a
mijacquot <mickael.jacquot@etu.uca.fr>
2023-06-24 20:07:47 +0200

    fix

--- src/main/java/com/example/dartsapi/mappers/games/GameEntityToDocumentMapper.java
+++ src/main/java/com/example/dartsapi/mappers/games/GameEntityToDocumentMapper.java
@@ -4,13 +4,23 @@ import com.example.dartsapi.entities.GameEntity;
 import org.bson.Document;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class GameEntityToDocumentMapper {
 
     public static Document gameEntityToDocument(GameEntity entity) {
         Document document = new Document();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        String formattedDate = dateFormat.format(entity.getDate());
+        // Conversion de Date vers LocalDateTime
+        LocalDateTime localDateTime = entity.getDate().toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+
+        // Utilisation de DateTimeFormatter pour formater la date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String formattedDate = localDateTime.format(formatter);
+
         document.put("date", formattedDate);
         document.put("userId", entity.getUserId());
         document.put("scores", entity.getScores());
