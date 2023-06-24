169b143bbe03f9ae791f9d3d148ac091174ac4f1
mijacquot <mickael.jacquot@etu.uca.fr>
2023-06-24 20:13:41 +0200

    fix

--- src/main/java/com/example/dartsapi/mappers/games/GameEntityToDocumentMapper.java
+++ src/main/java/com/example/dartsapi/mappers/games/GameEntityToDocumentMapper.java
@@ -3,15 +3,11 @@ package com.example.dartsapi.mappers.games;
 import com.example.dartsapi.entities.GameEntity;
 import org.bson.Document;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 public class GameEntityToDocumentMapper {
 
     public static Document gameEntityToDocument(GameEntity entity) {
         Document document = new Document();
+        document.put("date", entity.getDate());
         document.put("userId", entity.getUserId());
         document.put("scores", entity.getScores());
         document.put("status", entity.getStatus());
